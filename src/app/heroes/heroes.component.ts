d8a571886393149ff939f3f210ad735db9f912cc
Lawrence <lawrenccee@gmail.com>
2018-02-19 09:32:25 -0800

    finish part 7

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,17 +8,11 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
-
   heroes: Hero[];
 
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
   constructor(private heroService: HeroService) { }
